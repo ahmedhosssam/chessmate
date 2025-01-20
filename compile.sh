55b06f7d6b9434b326776a438d42ac9a9768910a
Mohamed <mohamed1212213183@gmail.com>
2025-01-20 17:28:39 +0200

    TODO

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: switch to use make file instead of compile script
 g++ main.cpp src/square.cpp src/board.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 && ./a.out
 
 #if [ $? -eq 0 ]; then
